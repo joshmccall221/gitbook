51a59b8fd7a5433a7dad450ea707e295f8b2650b
jmccall <jmccall@pillartechnology.com>
2016-12-20 01:57:39 -0600

    gitbook updates

--- resume/go.sh
+++ resume/go.sh
@@ -1,1 +1,1 @@
-hackmyresume BUILD res.json TO out/resume.all -t node_modules/jsonresume-theme-elegant-leonth; open ./out/resume.html
+hackmyresume BUILD res.json TO ./resume.all -t node_modules/jsonresume-theme-elegant-leonth;cp ./resume.html ./index.html; open ./index.html
