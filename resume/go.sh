a291cf106e60d21dc290fc00345864dfb8161d2e
Josh McCall <joshmccall221@gmail.com>
2017-03-23 15:17:22 +0000

    gitbook updates

--- resume/go.sh
+++ resume/go.sh
@@ -1,1 +1,5 @@
-hackmyresume BUILD res.json TO ./resume.all -t node_modules/jsonresume-theme-elegant-leonth;cp ./resume.html ./index.html; open ./index.html
+sudo npm install hackmyresume -g;
+npm install jsonresume-theme-elegant-leonth;
+hackmyresume BUILD res.json TO ./resume.all -t ../node_modules/jsonresume-theme-elegant-leonth;
+cp ./resume.html ./index.html;
+open ./index.html
